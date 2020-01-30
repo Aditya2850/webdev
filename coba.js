aedb0e9fac55faede3efab5914478f70a0bc6fed
Aditya2850 <57937020+Aditya2850@users.noreply.github.com>
2020-01-30 21:03:36 +0800

    modul 5

---
+++ coba.js
@@ -0,0 +1,60 @@
+var variabel1 = 'asd'
+var variabel2 = 29.2
+
+let variabel3 = 12
+
+variabel4 = 22
+
+console.log(variabel4)
+console.log()
+
+function namaFungsi(params) {
+    
+}
+
+
+var namaFungsi2 = function (params) {
+    
+}
+
+var namaFungsi3 = (x) => {
+    console.log(x)
+}
+
+
+var tambah = (x, y) => {
+    return x + y
+}
+
+if(kondisi){
+
+
+}else if(){
+
+
+}else {
+
+
+}
+//cara1
+for(let i=0; i<10; i++){
+
+}
+
+//cara2
+ArrayX = [12, 98, 65]
+
+for (item in ArrayX) {
+
+}
+
+
+//cara3
+while(x<5){
+    x++
+}
+
+//cara4
+do{
+    console.log('halo')
+}while(x<5)
